1a25251acdaf5ef09f1a0e2b35bc6c916792a925
Muiumi <antontimkov2002@mail.ru>
2024-01-18 19:52:49 +0500

    CategoryController with working GET-requests (api/v1/category, api/v1/category/{id})

--- src/main/java/ru/rtstudy/educplatformsecurity/controller/CategoryController.java
+++ src/main/java/ru/rtstudy/educplatformsecurity/controller/CategoryController.java
@@ -7,20 +7,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.rtstudy.educplatformsecurity.dto.response.CourseShortDescriptionDto;
-import ru.rtstudy.educplatformsecurity.service.CourseService;
+import ru.rtstudy.educplatformsecurity.dto.response.CourseLongDescriptionDto;
+import ru.rtstudy.educplatformsecurity.service.CategoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/category")
 @RequiredArgsConstructor
 public class CategoryController {
 
-    private final CourseService courseService;
+    private final CategoryService categoryService;
 
-    @GetMapping("{id}")
-    public ResponseEntity<CourseShortDescriptionDto> getCoursesByCategoryId(@PathVariable("id") Long id) {
+    @GetMapping
+    public ResponseEntity<List<String>> getAllAvailableCategories() {
         return ResponseEntity
                 .status(HttpStatusCode.valueOf(200))
-                .body(courseService.findCourseByCategoryId(id));
+                .body(categoryService.getAllCategories());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<CourseLongDescriptionDto>> getCoursesByCategoryId(@PathVariable(name = "id") Long id) {
+        return ResponseEntity
+                .status(HttpStatusCode.valueOf(200))
+                .body(categoryService.getCoursesByCategory(id));
     }
 }
