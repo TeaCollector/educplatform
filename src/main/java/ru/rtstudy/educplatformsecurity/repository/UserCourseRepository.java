0ff332d5291bd102a9d618059bdb15294fbe2017
alexandrsergeev <sergeevbass@gmail.com>
2024-01-26 17:11:24 +0500

    Resolve conflict

--- src/main/java/ru/rtstudy/educplatformsecurity/repository/UserCourseRepository.java
+++ src/main/java/ru/rtstudy/educplatformsecurity/repository/UserCourseRepository.java
@@ -25,35 +25,6 @@ public interface UserCourseRepository extends JpaRepository<UserCourse, Long> {
                 AND us.mentorCourse = true
             """)
     boolean isMentorForCourse(Long userId, Long courseId);
-    @Query("""
-            select count(us) > 0
-            from UserCourse us
-            where us.user.id = :userId
-                AND us.course.id = :courseId
-                AND us.mentorCourse = true
-            """)
-    boolean isMentorForCourse(Long userId, Long courseId);
-
-    @Query("""
-            select count(us) > 0
-            from UserCourse us
-            join Lesson l
-            on l.id = :lessonId
-            where us.user.id = :userId
-                AND us.course.id = l.course.id
-                AND us.mentorCourse = true
-            """)
-    boolean isMentorForLesson(Long userId, Long lessonId);
-
-    @Modifying
-    @Query("""
-            update UserCourse us
-            set us.endCourse = true, us.finishCourse = current_timestamp 
-            where us.user.id = :userId
-            and us.course.id = :courseId
-            """)
-    void finishCourse(Long userId, Long courseId);
-
 
     @Modifying
     @Query("""
@@ -82,15 +53,11 @@ public interface UserCourseRepository extends JpaRepository<UserCourse, Long> {
             """)
     void finishCourse(Long userId, Long courseId);
 
-    boolean onCourse(Long courseId, Long userId);
-}
-
-    @Modifying
     @Query("""
-            update UserCourse us
-            set us.mentorCourse = true
-            where us.user.id = :userId
-            and us.course.id = :courseId
+            select count(uc) > 0
+            from UserCourse uc
+            where uc.course.id = :courseId
+            and uc.user.id = :userId
             """)
-    void upgradeToMentor(Long userId, Long courseId);
+    boolean onCourse(Long courseId, Long userId);
 }
